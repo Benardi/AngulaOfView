cd96f69561dff308f6a76db970874cd4ac0c900a
Benardi <benardinunes@gmail.com>
2017-10-11 23:22:45 -0300

    Button reveal added. Conditional styling through ngstyle and ngclass

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myNumber = 0;
+  show = false;
+  logs = [];
+
+  revealText(){
+    this.show = !this.show;
+    this.myNumber += 1;
+    this.logs.push(this.myNumber);
+  }
+
 }
