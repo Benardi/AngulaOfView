82163b0d8f5c9873d33cbd848ed9b96000a0780d
Benardi <benardinunes@gmail.com>
2018-03-28 22:59:08 -0300

    Counter service created.

--- services-assignment-start/src/app/app.component.ts
+++ services-assignment-start/src/app/app.component.ts
@@ -1,20 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {UsersService} from './users.service';
+import {CounterService} from './counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
-  activeUsers: string[];
-  inactiveUsers: string[];
+export class AppComponent {
+  num_transitions: number[] = this.counterService.current_trans;
 
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private counterService: CounterService) {}
 
-  ngOnInit() {
-    this.activeUsers = this.usersService.activeUsers;
-    this.inactiveUsers = this.usersService.inactiveUsers;
-  }
-  
 }
