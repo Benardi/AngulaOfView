36f69df1ef8b6d687198c9c56736c79d9d554f32
Benardi <benardinunes@gmail.com>
2018-04-04 12:08:07 -0300

    Form template synched with FormGroup

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projForm: FormGroup;
+
+  ngOnInit() {
+    this.projForm = new FormGroup({
+      'projectName': new FormControl(null),
+      'mail': new FormControl(null),
+      'projectStatus': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projForm);
+  }
 }
