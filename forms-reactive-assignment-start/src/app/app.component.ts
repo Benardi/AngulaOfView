0f3ab12b4faeeb39b128208e6ded7b699c5640ad
Benardi <benardinunes@gmail.com>
2018-04-04 12:29:23 -0300

    Status field now dropdown.Synchronous Validators added.

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,25 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   projForm: FormGroup;
+  forbbidenNames =  ['Test'];
 
   ngOnInit() {
     this.projForm = new FormGroup({
-      'projectName': new FormControl(null),
-      'mail': new FormControl(null),
+      'projectName': new FormControl(null,
+        [Validators.required, this.forbiddenProjName.bind(this)]),
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl(null)
     });
   }
 
   onSubmit() {
     console.log(this.projForm);
+  }
+
+  forbiddenProjName(control: FormControl): {[s: string]: boolean} {
+    if (this.forbbidenNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
