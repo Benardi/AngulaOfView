51d45ccfd15ed384d424f611674d121c5110d93d
Benardi <benardinunes@gmail.com>
2018-04-03 22:30:51 -0300

    Form submission now displayed at template

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  submitted = false;
+  user: {email: string, subscription: string, password: string};
+
+  constructor() {
+    this.user = {email: '', subscription: '', password: ''};
+  }
+
+  onSubmit() {
+    this.user.email = this.signupForm.value.mail;
+    this.user.subscription = this.signupForm.value.subscription;
+    this.user.password = this.signupForm.value.password;
+    this.submitted = true;
+
+    this.signupForm.reset();
+    this.signupForm.form.patchValue({subscription: 'advanced'});
+  }
+
 
 }
